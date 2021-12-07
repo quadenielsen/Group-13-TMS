e5d0284c0f11a5e68ed09b78267a830e825df968
quadenielsen <quade.nielsen@gmail.com>
2021-12-07 10:52:05 -0500

    admin data view in User project is now up to date with functionality in AdminWindow project

--- TMS_UI/User/User/Views/AdminViews/SelectDirectory_View.xaml.cs
+++ TMS_UI/User/User/Views/AdminViews/SelectDirectory_View.xaml.cs
@@ -46,13 +46,13 @@ namespace User.Views.AdminViews
                 cities = admin.Cities;
                 Carriers.DataContext = carriers;
                 Routes.DataContext = cities;
-                
+
             }
             catch (Exception ex)
             {
-                logger.Log(ex.Message);
+                logger.Log(ex.Message + ex.StackTrace + ex.TargetSite + ex.Source);
             }
-            
+
         }
 
 
@@ -65,14 +65,15 @@ namespace User.Views.AdminViews
                 {
                     admin.Carriers = carriers;
                     admin.Depots = depots;
-                    
-                    admin.UpdateCarrierData();
+                    string status;
+                    status = admin.UpdateCarrierData();
+                    Status.Text = status;
                 }
 
             }
             catch (Exception ex)
             {
-                logger.Log(ex.Message);
+                logger.Log(ex.Message + ex.StackTrace + ex.TargetSite + ex.Source);
             }
         }
 
@@ -92,10 +93,11 @@ namespace User.Views.AdminViews
             }
             catch (Exception ex)
             {
-                logger.Log(ex.Message);
+                logger.Log(ex.Message + ex.StackTrace + ex.TargetSite + ex.Source);
             }
-            
+
         }
+
 
 
         private void Depots_InitializingNewItem(object sender, InitializingNewItemEventArgs e)
@@ -103,7 +105,9 @@ namespace User.Views.AdminViews
             Depot depot = (Depot)e.NewItem;
             Carrier carrier = (Carrier)Carriers.SelectedItem;
             depot.CarrierName = carrier.CarrierName;
+            Depots.CurrentItem = depot;
         }
+
 
 
         private void ChooseLoggerFilepath_Click(object sender, RoutedEventArgs e)
@@ -139,6 +143,60 @@ namespace User.Views.AdminViews
 
             // Show contents of Log File in Textbox
             LogFileText.Text = File.ReadAllText(filepath);
+        }
+
+        private void IpText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void PortText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (IpText.Text != "" && PortText.Text != "" && UsernameText.Text != "" && PasswordText.Text != "" && CMPbutton.IsChecked == true)
+            {
+                //Assuming data is valid, can add more validation later
+
+                //Change app config settings
+                AppConfigClass.SetAppConfig("CMPip", IpText.Text);
+
+                //Change app config settings
+                AppConfigClass.SetAppConfig("CMPport", PortText.Text);
+
+                //Change app config settings
+                AppConfigClass.SetAppConfig("CMPusername", UsernameText.Text);
+
+                //Change app config settings
+                AppConfigClass.SetAppConfig("CMPpassword", PasswordText.Text);
+            }
+
+
+            if (IpText.Text != "" && PortText.Text != "" && UsernameText.Text != "" && PasswordText.Text != "" && TMSbutton.IsChecked == true)
+            {
+                //Assuming data is valid, can add more validation later
+
+                //Change app config settings
+                AppConfigClass.SetAppConfig("TMSip", IpText.Text);
+
+                //Change app config settings
+                AppConfigClass.SetAppConfig("TMSport", PortText.Text);
+
+                //Change app config settings
+                AppConfigClass.SetAppConfig("TMSusername", UsernameText.Text);
+
+                //Change app config settings
+                AppConfigClass.SetAppConfig("TMSpassword", PasswordText.Text);
+            }
+
+            //Error message if form not fully filled out
+            else
+            {
+                Error.Visibility = Visibility.Visible;
+            }
         }
     }
 }
